fe459eb3b9cca7b5d592f161ecfead73b4a0e12b
levietanh <levietanh03062011@gmail.com>
2022-12-05 23:40:30 +0700

    add assignment

--- Assignment 5_12_2022/Bai_1/main.cpp
+++ Assignment 5_12_2022/Bai_1/main.cpp
@@ -1,176 +1,293 @@
-#include <iostream>
-#include <string>
-
+﻿#include <iostream>
+#include<string.h>
 using namespace std;
 
-struct SinhVien {
-	string maSV;
-	string hoTen;
-	string gioiTinh;
-	//Ngay ngaySinh;
-	string diaChi;
-	string lop;
-	string khoa;
+struct Ngay
+{
+	int ngay, thang, nam;
+};
+struct SinhVien
+{
+	char MaSV[8];
+	char hoTen[50];
+	int gioiTinh;
+	Ngay ngaySinh;
+	char diaChi[100];
+	char lop[12];
+	char khoa[7];
+};
+struct Node
+{
+	SinhVien data;
+	Node* next;
 };
 
-//struct Ngay {
-//	int ngay, thang, nam;
-//};
-
-struct Node {
-	SinhVien* data;
-	Node* link;
+struct LinkedList
+{
+	Node* head;
+	Node* tail;
 };
-
-struct List {
-	Node* first;
-	Node* last;
-};
-
-void initDS(List* lDS) {
-	lDS->first = lDS->last = NULL;
+Node* CreateNode(SinhVien init_data)
+{
+	Node* node = new Node;
+	node->data = init_data;
+	node->next = NULL;
+	return node;
 }
 
-SinhVien* make(string ma, string ten, string gioi,/* Ngay ngay,*/ string dc, string lop, string khoa) {
-	SinhVien* p = new SinhVien;
-	p->maSV = ma;
-	p->hoTen = ten;
-	p->gioiTinh = gioi;
-	p->diaChi = dc;
-	p->lop = lop;
-	p->khoa = khoa;
-	return p;
+void CreateList(LinkedList& l)
+{
+	l.head = NULL;
+	l.tail = NULL;
 }
 
-Node* callNode(string ma, string ten, string gioi,/* Ngay ngay,*/ string dc, string lop, string khoa) {
-	Node* p;
-	p = new Node;
-	p->data = make(ma, ten, gioi, dc, lop, khoa);
-	return p;
-}
-
-List* addNode(List* lDS, Node* p) {
-	if (lDS->first == NULL) {
-		lDS->first = p;
-		lDS->last = p;
+void AddHead(LinkedList& l, Node* node)
+{
+	if (l.head == NULL)
+	{
+		l.head = node;
+		l.tail = node;
 	}
-	else {
-		lDS->last->link = p;
-		lDS->last = p;
-		lDS->last->link = NULL;
+	else
+	{
+		node->next = l.head;
+		l.head = node;
 	}
-	return lDS;
 }
 
-List* attachNode(List* lDS, string ma, string ten, string gioi,/* Ngay ngay,*/ string dc, string lop, string khoa) {
-	Node* pDS = callNode(ma, ten, gioi,/* ngay,*/ dc, lop, khoa);
-	if (pDS == NULL) return lDS;
-	lDS = addNode(lDS, pDS);
-	return lDS;
+void AddTail(LinkedList& l, Node* node)
+{
+	if (l.head == NULL)
+	{
+		l.head = node;
+		l.tail = node;
+	}
+	else
+	{
+		l.tail->next = node;
+		l.tail = node;
+	}
 }
 
-void NhapDsSV(List* lDS) {
-	string ma;
-	string ten;
-	string gioi;
-	/*Ngay ngay;*/
-	string dc;
-	string lop;
-	string khoa;
+void InsertAfterQ(LinkedList& l, Node* p, Node* q)
+{
+	if (q != NULL)
+	{
+		cout << 1 << ":T";
+		p->next = q->next;
+		q->next = p;
+		if (l.tail == q)
+			l.tail = p;
+	}
+	else
+		AddTail(l, p);
+}
+
+int RemoveHead(LinkedList& l, SinhVien& x)
+{
+	if (l.head != NULL)
+	{
+		Node* node = l.head;
+		x = node->data;
+		l.head = node->next;
+		delete node;
+		if (l.head == NULL)
+			l.tail = NULL;
+		return 1;
+	}
+	return 0;
+}
+
+int RemoveAfterQ(LinkedList& l, Node* q, SinhVien& x)
+{
+	if (q != NULL)
+	{
+		Node* p = q->next;
+		if (p != NULL)
+		{
+			if (l.tail == p)
+				l.tail = q;
+			q->next = p->next;
+			x = p->data;
+			delete p;
+			return 1;
+		}
+		return 0;
+	}
+	return 0;
+}
+
+Node* GetNode(LinkedList l, int index)
+{
+	Node* node = l.head;
 	int i = 0;
-	cout << "Bat dau nhap DS Sinh Vien (Nhap MSSV 0 de ket thuc): " << endl;
-	do {
+	while (node != NULL && i != index)
+	{
+		node = node->next;
 		i++;
-		cout << "Nhap SV thu " << i << endl;
-		cout << "Nhap MSSV: ";
-		getline(cin, ma);
-		if (ma == "0") break;
-		cout << "Nhap ho ten SV: ";
-		fflush(stdin);
-		getline(cin, ten);
-		cout << "Nhap gioi tinh: ";
-		fflush(stdin);
-		getline(cin, gioi);
-		/*cout << "Nhap ngay thang nam sinh (lan luot): ";
-		cin >> ngay.ngay >> ngay.thang >> ngay.nam;*/
-		cout << "Nhap dia chi SV: ";
-		fflush(stdin);
-		getline(cin, dc);
-		cout << "Nhap lop SV: ";
-		fflush(stdin);
-		getline(cin, lop);
-		cout << "Nhap khoa SV: ";
-		fflush(stdin);
-		getline(cin, khoa);
-		lDS = attachNode(lDS, ma, ten, gioi,/* ngay,*/ dc, lop, khoa);
-	} while (ma != "0");
-	cout << "Ket thuc nhap." << endl;
-}
-
-void swapNode(List* lDS, Node* p, Node* q) {
-	Node* k = new Node;
-	Node* m = lDS->first;
-	Node* n = lDS->first;
-	if (m == NULL) return;
-	while (m != p) {
-		if (m->link == p) break;
-		else m = m->link;
 	}
-	while (n != q) {
-		if (n->link == q) break;
-		else n = n->link;
-	}
-	k->link = p->link;
-	p->link = q->link;
-	q->link = k->link;
-	if (m != p) m->link = q;
-	if (n != q) n->link = p;
+	if (i == index && node != NULL)
+		return node;
+	return NULL;
 }
-
-void sapXepDS(List* lDS) {
-	Node* k, * p, * q;
-	k = p = lDS->first;
-	q = p->link;
-	while (k->link->link != NULL) {
-		do {
-			if (p->data->maSV > q->data->maSV) p = q;
-			q = q->link;
-		} while (q != NULL);
-		swapNode(lDS, p, k);
-		k = k->link;
-		p = k;
+void XuatNode(Node* node)
+{
+	cout << node->data.MaSV << ' '
+		<< node->data.hoTen << ' '
+		<< node->data.gioiTinh << ' '
+		<< node->data.ngaySinh.nam << "/"
+		<< node->data.ngaySinh.thang << "/"
+		<< node->data.ngaySinh.nam << ' '
+		<< node->data.diaChi << ' '
+		<< node->data.lop << ' '
+		<< node->data.khoa
+		<< endl;
+}
+void PrintList(LinkedList l)
+{
+	if (l.head != NULL)
+	{
+		Node* node = l.head;
+		while (node != NULL)
+		{
+			XuatNode(node);
+			node = node->next;
+		}
 	}
 }
 
-void inDS(List* lDS) {
-	if (lDS == NULL) return;
-	Node* p = lDS->first;
 
-	cout << "Danh sach Sinh Vien: " << endl;
-	/*while (true) {
-		cout << p->data->hoTen << " - " << p->data->maSV << endl;
+Node* SearchInsert(LinkedList l, SinhVien x)
+{
+	Node* node = l.head;
+	while (node != NULL && strcmp(node->data.MaSV, x.MaSV) < 0)
+	{
 
-		if (p == lDS->last) return;
-		p = p->link;
-	}*/
-	while (p != NULL) {
-		cout << p->data->hoTen << " - " << p->data->maSV << endl;
-		p = p->link;
+		node = node->next;
+	}
+	if (node != NULL)
+		return node;
+	return NULL;
+}
+void SortList(LinkedList& list)
+{
+	// for loop thứ nhất
+	for (Node* pTmp = list.head; pTmp != NULL; pTmp = pTmp->next)
+	{
+		//for loop thứ hai
+		for (Node* pTmp2 = pTmp->next; pTmp2 != NULL; pTmp2 = pTmp2->next)
+		{
+			if (strcmp(pTmp->data.MaSV, pTmp2->data.MaSV) > 0) // nếu giá trị trước > giá trị sau thì hoán đổi hai vị trí
+			{
+				SinhVien tmp = pTmp->data;
+				pTmp->data = pTmp2->data;
+				pTmp2->data = tmp;
+			}
+		}
 	}
 }
-
-void themSV(List* lDS, string ma, string ten, string gioi,/* Ngay ngay,*/ string dc, string lop, string khoa) {
-	Node* pDS = callNode(ma, ten, gioi,/* ngay,*/ dc, lop, khoa);
-	if (pDS == NULL) return;
-	addNode(lDS, pDS);
-	sapXepDS(lDS);
+int Length(LinkedList l)
+{
+	int count = 0;
+	Node* node = l.head;
+	while (node != NULL)
+	{
+		count++;
+		node = node->next;
+	}
+	return count;
 }
 
-int main() {
-	List DS1;
-	initDS(&DS1);
-	NhapDsSV(&DS1);
-	sapXepDS(&DS1);
-	inDS(&DS1);
+void DestroyList(LinkedList& l)
+{
+	SinhVien x;
+	Node* node = l.head;
+	while (node != NULL)
+	{
+		RemoveHead(l, x);
+		node = l.head;
+	}
+	l.tail = NULL;
+}
+void NhapSv(SinhVien& sv)
+{
+	cout << "Nhap MaSV: "; fflush(stdin); gets(sv.MaSV);
+	cout << "Nhap HoTen: "; fflush(stdin); gets(sv.hoTen);
+	cout << "Nhap gioi tinh: (Nam:1 Nu:0)"; cin >> sv.gioiTinh;
+	cout << "Nhap ngay sinh: "; fflush(stdin); cin >> sv.ngaySinh.ngay;
+	cout << "Nhap thang sinh: "; fflush(stdin); cin >> sv.ngaySinh.thang;
+	cout << "Nhap nam sinh: "; fflush(stdin); cin >> sv.ngaySinh.nam;
+	cout << "Nhap dia chi: "; fflush(stdin); gets(sv.diaChi);
+	cout << "Nhap lop: "; fflush(stdin); gets(sv.lop);
+	cout << "Nhap khoa: "; fflush(stdin); gets(sv.khoa);
+}
+void SinhViensameNgaySinh(LinkedList list)
+{
+	int len = Length(list);
+	bool Check[len] = {};
+	int i = 0, j;
+	bool checkNull = false;
+	cout << "Danh sach Sinh Vien cung ngay sinh" << endl;
+	for (Node* pTmp = list.head; pTmp != NULL; pTmp = pTmp->next)
+	{//for loop thứ hai
+		bool checkdelete = false;
+		j = i + 1;
+		if (!Check[i])
+		{
+			XuatNode(pTmp);
+		}
+		for (Node* pTmp2 = pTmp->next; pTmp2 != NULL; pTmp2 = pTmp2->next)
+		{
+			if (pTmp->data.ngaySinh.ngay == pTmp2->data.ngaySinh.ngay and !Check[j]) // nếu giá trị trước > giá trị sau thì hoán đổi hai vị trí
+			{
+				RemoveAfterQ(list, pTmp2, pTmp->data);
+				XuatNode(pTmp2);
+				Check[j] = true;
+				checkNull = true;
+				checkdelete = true;
+			}
+			j++;
+		}
+		if (checkdelete)
+		{
+			RemoveAfterQ(list, pTmp, pTmp->data);
+		}
+		i++;
+		cout << "-------------------------------------------------------" << endl;
+
+	}
+	if (!checkNull)
+	{
+		cout << "khong co sinh vien nao cung ngay sinh" << endl;
+	}
+}
+int main()
+{
+	// Create a linked list
+	LinkedList list;
+	CreateList(list);
+
+	// Add sample data to list
+	Node* node;
+	SinhVien sv;
+	int len1;
+	cout << "Nhap vao so luong danh sach: "; cin >> len1;
+	for (auto i = 1; i <= len1; i++)
+	{
+		NhapSv(sv);
+		// Create new node with init data is sv
+		node = CreateNode(sv);
+		AddTail(list, node);
+	}
+	SortList(list);
+	// Print list
+	PrintList(list);
+	cout << endl;
+
+	// Get list's length
+	int len = Length(list);
+	cout << "Length of list: " << len << endl;
+	DestroyList(list);
+
 	return 0;
 }
